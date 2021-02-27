302414f23902d12f275df70517aee0ec12307c1c
vitoremanueldev <veslima3@gmail.com>
2021-02-26 23:48:14 -0300

    Rota do status e formatação de numero e data

--- src/main/java/br/com/spring/mvc/repository/PedidoRepository.java
+++ src/main/java/br/com/spring/mvc/repository/PedidoRepository.java
@@ -1,11 +1,17 @@
 package br.com.spring.mvc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.spring.mvc.model.Pedido;
+import br.com.spring.mvc.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+
+	List<Pedido> findByStatus(StatusPedido status);
+
 
 }
